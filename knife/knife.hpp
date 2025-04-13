c0b805e4088807b4f6e9a63fbd56d9a707a50d24
A-Davron <davron.abidov06@gmail.com>
2025-04-13 13:34:07 +0500

    Fixed some problems with constructors, added "using namespace constructor"

--- knife/knife.hpp
+++ knife/knife.hpp
@@ -2,7 +2,9 @@
 #include "sorting/quick_sort.hpp"
 #include "sorting/merge_sort.hpp"
 #include "containers/stack.hpp"
+#include "containers/vector.h"
 
 namespace knife {
     using namespace sorting;
+    using namespace containers;
 } //knife namespace
